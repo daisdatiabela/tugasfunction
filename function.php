fe4019df06a9a949b0b7a9877042ca112754bc52
daisdatiabela <syadais02@gmail.com>
2024-11-30 09:11:01 +0700

    Add files via upload

---
+++ function.php
@@ -0,0 +1,17 @@
+<?php 
+//function yang tidak mengembalikan//
+
+function menampilkanNama(){
+    echo "hai namaku Dais";
+
+    echo "<br>";
+}
+//function mengembalikan Nilai//
+function pengurangan ($nilai1, $nilai2){
+    $hasil = $nilai1 - $nilai2;
+    return  $hasil;
+}
+
+ pengurangan(236,135);
+
+ 
